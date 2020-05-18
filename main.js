ee0b791a88091d3180ada9cfb3409b0a99aadd39
David <41311088+biagiola@users.noreply.github.com>
2020-05-18 16:29:44 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth-Token'] = 
+ 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'; 
+
 // GET REQUEST
 function getTodos() {
   /*axios({
@@ -70,14 +74,43 @@ function getData() {
       .catch(err => console.error(err));
 }
 
+/*
+
+*/
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'sometoken' //some jwt 
+    }
+  }
+  
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title: 'New Todo',
+    completed: false
+  }, 
+    config
+  )
+  .then(res => showOutput(res))
+  .catch(err => console.error(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method: 'post',
+    url:   'https://jsonplaceholder.typicode.com/todos',
+    data: {
+      title: 'Hello World'
+    },
+    transformResponse: axios.defaults.transformResponse.concat( data => {
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  };
+
+  axios(options).then(res => showOutput(res));
 }
 
 // ERROR HANDLING
